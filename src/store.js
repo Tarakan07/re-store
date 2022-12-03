73146db5a39a4aff9e38fb5b99f7851e686607c1
Boris <boris_galunga@mail.ru>
2022-12-03 16:09:09 +0300

    Middleware. По сравнение с предыдущим коммитом, этот метод не меняет весь store, а меняет только функцию dispatch. И именно это используется в продакшене

--- src/store.js
+++ src/store.js
@@ -1,7 +1,51 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from "redux";
 
-import reducer from './reducers';
+import reducer from "./reducers";
 
-const store = createStore(reducer);
+const logMiddleware = (store) => (dispatch) => (action) => {
+	console.log(action, store);
+	return dispatch(action);
+};
+const stringMeddleware = () => (dispatch) => (action) => {
+	if (typeof action === "string") {
+		return dispatch({
+			type: action,
+		});
+	}
+	return dispatch(action);
+};
+// const stringEnhancer =
+// 	(createStore) =>
+// 	(...args) => {
+// 		console.log(...args);
+// 		const store = createStore(...args);
+// 		const originalDispatch = store.dispatch;
+// 		store.dispatch = (action) => {
+// 			if (typeof action === "string") {
+// 				return originalDispatch({
+// 					type: action,
+// 				});
+// 			}
+// 			return originalDispatch(action);
+// 		};
+// 		return store;
+// 	};
+// const logEnhancer =
+// 	(createStore) =>
+// 	(...args) => {
+// 		console.log(...args);
+// 		const store = createStore(...args);
+// 		const originalDispatch = store.dispatch;
+// 		store.dispatch = (action) => {
+// 			console.log(action);
+// 			return originalDispatch(action);
+// 		};
+// 		return store;
+// 	};
+const store = createStore(
+	reducer,
+	applyMiddleware(stringMeddleware, logMiddleware)
+);
 
+store.dispatch({ type: "hello world" });
 export default store;
